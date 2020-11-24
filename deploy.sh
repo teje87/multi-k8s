8445b18c13128b0a5f9fd59a00144c6fe3a819c1
m.tejero@e-coordina.com <m.tejero@e-coordina.com>
2020-11-24 14:17:26 +0100

    Minor fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-docker build -t teje87/multi-client -f ./client/Dockerfile ./client
-docker build -t teje87/multi-server -f ./server/Dockerfile ./server
-docker build -t teje87/multi-worker -f ./worker/Dockerfile ./worker
-docker push teje87/multi-client
-docker push teje87multi-server
-docker push teje87/multi-worker
+docker build -t teje87/multi-client:latest -t teje87/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t teje87/multi-server:latest -t teje87/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t teje87/multi-worker:latest -t teje87/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push teje87/multi-client:latest
+docker push teje87multi-server:latest
+docker push teje87/multi-worker:latest
+
+docker push teje87/multi-client:$SHA
+docker push teje87multi-server:$SHA
+docker push teje87/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=teje87/multi-server
+kubectl set image deployments/server-deployment server=teje87/multi-server:$SHA
+kubectl set image deployments/client-deployment client=teje87/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=teje87/multi-worker:$SHA
